4581857863df82d322a9b97e02f4d62e440453cf
Quarke <tflanagan94@me.com>
2017-12-04 13:05:59 -0500

    Opening port for heroku control

--- index.js
+++ index.js
@@ -107,4 +107,4 @@ router.route('/meta')
 
 app.use('/api', router)
 app.use('/', express.static('pages'))
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
